7b4a090fa00f74e3eb5199be64826592c232dd16
FirstVideos Group <apps@firstvideosgroup.eu>
2026-02-26 06:07:41 +0100

    fix(nxn-engine): egységes konzol színkódok a logolásban

--- nxn-engine/shared.lua
+++ nxn-engine/shared.lua
@@ -8,16 +8,21 @@ NXN.Engine = NXN.Engine or {}
 ---@param msg string
 function NXN.Engine.Log(msg)
     if Config.Debug then
-        print(('[nxn-engine] [DEBUG] %s'):format(tostring(msg)))
+        print(('^9[nxn-engine]^7 ^5[DEBUG]^7 %s'):format(tostring(msg)))
     end
 end
 
 ---@param msg string
 function NXN.Engine.Info(msg)
-    print(('[nxn-engine] [INFO] %s'):format(tostring(msg)))
+    print(('^9[nxn-engine]^7 ^4[INFO]^7 %s'):format(tostring(msg)))
 end
 
 ---@param msg string
 function NXN.Engine.Warn(msg)
-    print(('[nxn-engine] [WARN] %s'):format(tostring(msg)))
+    print(('^9[nxn-engine]^7 ^3[WARN]^7 %s'):format(tostring(msg)))
+end
+
+---@param msg string
+function NXN.Engine.Error(msg)
+    print(('^9[nxn-engine]^7 ^1[HIBA]^7 %s'):format(tostring(msg)))
 end
