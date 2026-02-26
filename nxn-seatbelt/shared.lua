5745e81f82dcf599eabb81ebc74a606de47c92ab
FirstVideos Group <apps@firstvideosgroup.eu>
2026-02-26 06:13:18 +0100

    fix(nxn-seatbelt): egységes konzol színkódok a logolásban

--- nxn-seatbelt/shared.lua
+++ nxn-seatbelt/shared.lua
@@ -8,16 +8,21 @@ NXN.Seatbelt = NXN.Seatbelt or {}
 ---@param msg string
 function NXN.Seatbelt.Log(msg)
     if Config.Debug then
-        print(('[nxn-seatbelt] [DEBUG] %s'):format(tostring(msg)))
+        print(('^9[nxn-seatbelt]^7 ^5[DEBUG]^7 %s'):format(tostring(msg)))
     end
 end
 
 ---@param msg string
 function NXN.Seatbelt.Info(msg)
-    print(('[nxn-seatbelt] [INFO] %s'):format(tostring(msg)))
+    print(('^9[nxn-seatbelt]^7 ^4[INFO]^7 %s'):format(tostring(msg)))
 end
 
 ---@param msg string
 function NXN.Seatbelt.Warn(msg)
-    print(('[nxn-seatbelt] [WARN] %s'):format(tostring(msg)))
+    print(('^9[nxn-seatbelt]^7 ^3[WARN]^7 %s'):format(tostring(msg)))
+end
+
+---@param msg string
+function NXN.Seatbelt.Error(msg)
+    print(('^9[nxn-seatbelt]^7 ^1[HIBA]^7 %s'):format(tostring(msg)))
 end
