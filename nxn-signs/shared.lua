a08ca8c85ed8a3e880a03186712b0c5e4db64508
FirstVideos Group <apps@firstvideosgroup.eu>
2026-02-26 06:13:37 +0100

    fix(nxn-signs): egységes konzol színkódok a logolásban

--- nxn-signs/shared.lua
+++ nxn-signs/shared.lua
@@ -8,16 +8,21 @@ NXN.Signs = NXN.Signs or {}
 ---@param msg string
 function NXN.Signs.Log(msg)
     if Config.Debug then
-        print(('[nxn-signs] [DEBUG] %s'):format(tostring(msg)))
+        print(('^9[nxn-signs]^7 ^5[DEBUG]^7 %s'):format(tostring(msg)))
     end
 end
 
 ---@param msg string
 function NXN.Signs.Info(msg)
-    print(('[nxn-signs] [INFO] %s'):format(tostring(msg)))
+    print(('^9[nxn-signs]^7 ^4[INFO]^7 %s'):format(tostring(msg)))
 end
 
 ---@param msg string
 function NXN.Signs.Warn(msg)
-    print(('[nxn-signs] [WARN] %s'):format(tostring(msg)))
+    print(('^9[nxn-signs]^7 ^3[WARN]^7 %s'):format(tostring(msg)))
+end
+
+---@param msg string
+function NXN.Signs.Error(msg)
+    print(('^9[nxn-signs]^7 ^1[HIBA]^7 %s'):format(tostring(msg)))
 end
