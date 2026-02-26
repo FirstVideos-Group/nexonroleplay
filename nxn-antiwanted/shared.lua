d400fe454c8360cbac5a0d13484498094b0fa22b
FirstVideos Group <apps@firstvideosgroup.eu>
2026-02-26 06:10:04 +0100

    fix(nxn-antiwanted): egységes konzol színkódok a logolásban

--- nxn-antiwanted/shared.lua
+++ nxn-antiwanted/shared.lua
@@ -9,6 +9,21 @@ NXN.AntiWanted = {}
 ---@param msg string
 function NXN.AntiWanted.Log(msg)
     if Config and Config.Debug then
-        print(('[nxn-antiwanted] [DEBUG] %s'):format(tostring(msg)))
+        print(('^9[nxn-antiwanted]^7 ^5[DEBUG]^7 %s'):format(tostring(msg)))
     end
+end
+
+---@param msg string
+function NXN.AntiWanted.Info(msg)
+    print(('^9[nxn-antiwanted]^7 ^4[INFO]^7 %s'):format(tostring(msg)))
+end
+
+---@param msg string
+function NXN.AntiWanted.Warn(msg)
+    print(('^9[nxn-antiwanted]^7 ^3[WARN]^7 %s'):format(tostring(msg)))
+end
+
+---@param msg string
+function NXN.AntiWanted.Error(msg)
+    print(('^9[nxn-antiwanted]^7 ^1[HIBA]^7 %s'):format(tostring(msg)))
 end
