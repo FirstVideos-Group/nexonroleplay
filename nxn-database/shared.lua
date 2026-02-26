3297321532844f2422b5dfd3fda6e2bc7e52a55b
FirstVideos Group <apps@firstvideosgroup.eu>
2026-02-26 06:04:13 +0100

    fix(nxn-database): egységes konzol színkódok a logolásban

--- nxn-database/shared.lua
+++ nxn-database/shared.lua
@@ -10,24 +10,24 @@ NXN.DB      = NXN.DB or {}
 ---@param msg string
 function NXN.DB.Log(msg)
     if Config.Debug then
-        print(('[nxn-database] [DEBUG] %s'):format(tostring(msg)))
+        print(('^9[nxn-database]^7 ^5[DEBUG]^7 %s'):format(tostring(msg)))
     end
 end
 
 --- Info log – mindig megjelenik
 ---@param msg string
 function NXN.DB.Info(msg)
-    print(('[nxn-database] [INFO] %s'):format(tostring(msg)))
+    print(('^9[nxn-database]^7 ^4[INFO]^7 %s'):format(tostring(msg)))
 end
 
 --- Warn log
 ---@param msg string
 function NXN.DB.Warn(msg)
-    print(('[nxn-database] [WARN] %s'):format(tostring(msg)))
+    print(('^9[nxn-database]^7 ^3[WARN]^7 %s'):format(tostring(msg)))
 end
 
 --- Error log
 ---@param msg string
 function NXN.DB.Error(msg)
-    print(('[nxn-database] [ERROR] %s'):format(tostring(msg)))
+    print(('^9[nxn-database]^7 ^1[HIBA]^7 %s'):format(tostring(msg)))
 end
