bb3fd50e9eff4fd6ca1a6696e90faa4d7b35155b
FirstVideos Group <apps@firstvideosgroup.eu>
2026-02-26 06:13:48 +0100

    fix(nxn-vehicle-hud): egységes konzol színkódok a logolásban

--- nxn-vehicle-hud/shared.lua
+++ nxn-vehicle-hud/shared.lua
@@ -9,18 +9,24 @@ NXN.VehHUD  = NXN.VehHUD or {}
 ---@param msg string
 function NXN.VehHUD.Log(msg)
     if Config.Debug then
-        print(('[nxn-vehicle-hud] [DEBUG] %s'):format(tostring(msg)))
+        print(('^9[nxn-vehicle-hud]^7 ^5[DEBUG]^7 %s'):format(tostring(msg)))
     end
 end
 
 --- Info log
 ---@param msg string
 function NXN.VehHUD.Info(msg)
-    print(('[nxn-vehicle-hud] [INFO] %s'):format(tostring(msg)))
+    print(('^9[nxn-vehicle-hud]^7 ^4[INFO]^7 %s'):format(tostring(msg)))
 end
 
 --- Warn log
 ---@param msg string
 function NXN.VehHUD.Warn(msg)
-    print(('[nxn-vehicle-hud] [WARN] %s'):format(tostring(msg)))
+    print(('^9[nxn-vehicle-hud]^7 ^3[WARN]^7 %s'):format(tostring(msg)))
+end
+
+--- Error log
+---@param msg string
+function NXN.VehHUD.Error(msg)
+    print(('^9[nxn-vehicle-hud]^7 ^1[HIBA]^7 %s'):format(tostring(msg)))
 end
