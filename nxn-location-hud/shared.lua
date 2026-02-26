a04b7831bb31072aa660f3e445049cc499705ef1
FirstVideos Group <apps@firstvideosgroup.eu>
2026-02-26 06:12:25 +0100

    fix(nxn-location-hud): egységes konzol színkódok a logolásban

--- nxn-location-hud/shared.lua
+++ nxn-location-hud/shared.lua
@@ -9,18 +9,24 @@ NXN.LocHUD   = NXN.LocHUD or {}
 ---@param msg string
 function NXN.LocHUD.Log(msg)
     if Config.Debug then
-        print(('[nxn-location-hud] [DEBUG] %s'):format(tostring(msg)))
+        print(('^9[nxn-location-hud]^7 ^5[DEBUG]^7 %s'):format(tostring(msg)))
     end
 end
 
 --- Info log
 ---@param msg string
 function NXN.LocHUD.Info(msg)
-    print(('[nxn-location-hud] [INFO] %s'):format(tostring(msg)))
+    print(('^9[nxn-location-hud]^7 ^4[INFO]^7 %s'):format(tostring(msg)))
 end
 
 --- Warn log
 ---@param msg string
 function NXN.LocHUD.Warn(msg)
-    print(('[nxn-location-hud] [WARN] %s'):format(tostring(msg)))
+    print(('^9[nxn-location-hud]^7 ^3[WARN]^7 %s'):format(tostring(msg)))
+end
+
+--- Error log
+---@param msg string
+function NXN.LocHUD.Error(msg)
+    print(('^9[nxn-location-hud]^7 ^1[HIBA]^7 %s'):format(tostring(msg)))
 end
