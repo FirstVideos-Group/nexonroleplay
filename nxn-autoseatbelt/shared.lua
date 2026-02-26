15332afcfc3b08b89fccc340aee6edb9933c5a93
FirstVideos Group <apps@firstvideosgroup.eu>
2026-02-26 06:06:46 +0100

    fix(nxn-autoseatbelt): egységes konzol színkódok a logolásban

--- nxn-autoseatbelt/shared.lua
+++ nxn-autoseatbelt/shared.lua
@@ -8,16 +8,21 @@ NXN.AutoSeatbelt = NXN.AutoSeatbelt or {}
 ---@param msg string
 function NXN.AutoSeatbelt.Log(msg)
     if Config.Debug then
-        print(('[nxn-autoseatbelt] [DEBUG] %s'):format(tostring(msg)))
+        print(('^9[nxn-autoseatbelt]^7 ^5[DEBUG]^7 %s'):format(tostring(msg)))
     end
 end
 
 ---@param msg string
 function NXN.AutoSeatbelt.Info(msg)
-    print(('[nxn-autoseatbelt] [INFO] %s'):format(tostring(msg)))
+    print(('^9[nxn-autoseatbelt]^7 ^4[INFO]^7 %s'):format(tostring(msg)))
 end
 
 ---@param msg string
 function NXN.AutoSeatbelt.Warn(msg)
-    print(('[nxn-autoseatbelt] [WARN] %s'):format(tostring(msg)))
+    print(('^9[nxn-autoseatbelt]^7 ^3[WARN]^7 %s'):format(tostring(msg)))
+end
+
+---@param msg string
+function NXN.AutoSeatbelt.Error(msg)
+    print(('^9[nxn-autoseatbelt]^7 ^1[HIBA]^7 %s'):format(tostring(msg)))
 end
