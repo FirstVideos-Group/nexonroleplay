d94042b9fab90f37c11fba902ce0449d07498e08
FirstVideos Group <apps@firstvideosgroup.eu>
2026-02-26 06:11:30 +0100

    fix(nxn-identity): egységes konzol színkódok a logolásban

--- nxn-identity/shared.lua
+++ nxn-identity/shared.lua
@@ -8,16 +8,21 @@ NXN.Identity = NXN.Identity or {}
 ---@param msg string
 function NXN.Identity.Log(msg)
     if Config.Debug then
-        print(('[nxn-identity] [DEBUG] %s'):format(tostring(msg)))
+        print(('^9[nxn-identity]^7 ^5[DEBUG]^7 %s'):format(tostring(msg)))
     end
 end
 
 ---@param msg string
 function NXN.Identity.Info(msg)
-    print(('[nxn-identity] [INFO] %s'):format(tostring(msg)))
+    print(('^9[nxn-identity]^7 ^4[INFO]^7 %s'):format(tostring(msg)))
 end
 
 ---@param msg string
 function NXN.Identity.Warn(msg)
-    print(('[nxn-identity] [WARN] %s'):format(tostring(msg)))
+    print(('^9[nxn-identity]^7 ^3[WARN]^7 %s'):format(tostring(msg)))
+end
+
+---@param msg string
+function NXN.Identity.Error(msg)
+    print(('^9[nxn-identity]^7 ^1[HIBA]^7 %s'):format(tostring(msg)))
 end
