307bc6aea90da335f7ff43ec15732f5c5687e90e
FirstVideos Group <apps@firstvideosgroup.eu>
2026-02-26 06:12:57 +0100

    fix(nxn-notify): egységes konzol színkódok a logolásban

--- nxn-notify/shared.lua
+++ nxn-notify/shared.lua
@@ -9,24 +9,24 @@ NXN.Notify   = NXN.Notify or {}
 ---@param msg string
 function NXN.Notify.Log(msg)
     if Config.Debug then
-        print(('[nxn-notify] [DEBUG] %s'):format(tostring(msg)))
+        print(('^9[nxn-notify]^7 ^5[DEBUG]^7 %s'):format(tostring(msg)))
     end
 end
 
 --- Info log
 ---@param msg string
 function NXN.Notify.Info(msg)
-    print(('[nxn-notify] [INFO] %s'):format(tostring(msg)))
+    print(('^9[nxn-notify]^7 ^4[INFO]^7 %s'):format(tostring(msg)))
 end
 
 --- Warn log
 ---@param msg string
 function NXN.Notify.Warn(msg)
-    print(('[nxn-notify] [WARN] %s'):format(tostring(msg)))
+    print(('^9[nxn-notify]^7 ^3[WARN]^7 %s'):format(tostring(msg)))
 end
 
 --- Error log
 ---@param msg string
 function NXN.Notify.Error(msg)
-    print(('[nxn-notify] [ERROR] %s'):format(tostring(msg)))
+    print(('^9[nxn-notify]^7 ^1[HIBA]^7 %s'):format(tostring(msg)))
 end
