30a92db94d03b21c249eb0338a9e7077011b91c8
FirstVideos Group <apps@firstvideosgroup.eu>
2026-02-26 06:12:34 +0100

    fix(nxn-minimap): egységes konzol színkódok a logolásban

--- nxn-minimap/shared.lua
+++ nxn-minimap/shared.lua
@@ -9,18 +9,24 @@ NXN.Minimap = NXN.Minimap or {}
 ---@param msg string
 function NXN.Minimap.Log(msg)
     if Config.Debug then
-        print(('[nxn-minimap] [DEBUG] %s'):format(tostring(msg)))
+        print(('^9[nxn-minimap]^7 ^5[DEBUG]^7 %s'):format(tostring(msg)))
     end
 end
 
 --- Info log
 ---@param msg string
 function NXN.Minimap.Info(msg)
-    print(('[nxn-minimap] [INFO] %s'):format(tostring(msg)))
+    print(('^9[nxn-minimap]^7 ^4[INFO]^7 %s'):format(tostring(msg)))
 end
 
 --- Warn log
 ---@param msg string
 function NXN.Minimap.Warn(msg)
-    print(('[nxn-minimap] [WARN] %s'):format(tostring(msg)))
+    print(('^9[nxn-minimap]^7 ^3[WARN]^7 %s'):format(tostring(msg)))
+end
+
+--- Error log
+---@param msg string
+function NXN.Minimap.Error(msg)
+    print(('^9[nxn-minimap]^7 ^1[HIBA]^7 %s'):format(tostring(msg)))
 end
